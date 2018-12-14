187c7f53cb4d4827635cf563ef64c0b1ea51bb3b
madeline-tatum <madeline.tatum@stu.jefferson.kyschool.us>
2018-12-14 08:31:37 -0500

    second commit

--- albumlist.py
+++ albumlist.py
@@ -1,4 +1,5 @@
 import csv
+from matplotlib import pyplot as plt
 
 filename = 'albumlist.csv'
 with open(filename) as f:
@@ -6,36 +7,33 @@ with open(filename) as f:
     header_row = next(reader)
     print(header_row)
 
-    # for index, column_header in enumerate(header_row):
-    #     print(index, column_header)
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
 
     numbers = []
     for row in reader:
         numbers.append(row[0])
-    print(numbers)
 
     years = []
     for row in reader:
         years.append(row[1])
-    print(years)
 
     albums = []
     for row in reader:
         albums.append(row[2])
-    print(albums)
 
     artists = []
     for row in reader:
         artists.append(row[3])
-    print(artists)
 
     genres = []
     for row in reader:
         genres.append(row[4])
-    print(genres)
 
     subgenres = []
     for row in reader:
         subgenres.append(row[5])
-    print(subgenres)
 
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.bar(numbers, height=500)
+plt.show()
