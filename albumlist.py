325e4a85b8d9525006d9e02327edcda38288ae2e
madeline-tatum <madeline.tatum@stu.jefferson.kyschool.us>
2018-12-19 08:31:17 -0500

    final commit

--- albumlist.py
+++ albumlist.py
@@ -25,14 +25,23 @@ with open(filename) as f:
         genres.append(row[4])
         subgenres.append(row[5])
 
-mc_artists = collections.Counter(artists).most_common(10)
-print(mc_artists)
+filename_2 = 'mc_artists.csv'
+with open(filename_2) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    print(header_row)
 
-x = ['The Beatles','Bob Dylan','The Rolling Stones','Bruce Springsteen','The Who','U2','Led Zeppelin','David Bowie','Radiohead','Elton John']
-y = [10,10,10,8,7,5,5,5,5,5]
-plt.plot(x,y)
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
+
+    them_artists = []
+    frequency = []
+    for row in reader:
+        them_artists.append(row[0])
+        frequency.append(row[1])
+
+plt.plot(them_artists,sorted(frequency))
 plt.title("Most Common Artists", fontsize=24)
 plt.xlabel("Artist", fontsize=14)
 plt.ylabel("Frequency of artist", fontsize=14)
 plt.show()
-
